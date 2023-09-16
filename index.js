9e54a3a31bbc9f9aa69127d0c3bd453e2ca7a528
yasar2385 <61082958+yasar2385@users.noreply.github.com>
2023-09-16 23:18:22 +0530

    1 file modified

--- index.js
+++ index.js
@@ -22,8 +22,39 @@ var Obj = {
 console.log(Obj.coll);
 console.log(Obj.last_lab());
 
-
-
-function label_Symbol(){
-  
+function label_Symbol() {}
+var count = 1;
+function getObjectChildren(node, Options = {}) {
+  try {
+    var { CheckEmpty, FetchClass } = Options;
+    var classList = [];
+    if (CheckEmpty) {
+      if (node.textContent == '') {
+        node.parentElement.removeChild(node);
+      }
+    } else {
+      count++;
+      if (node.hasChildren) {
+        console.log(node.children.map(getObjectChildren));
+        classList.push(node.children.map(getObjectChildren));
+      } else {
+        classList.push(node.className);
+      }
+      console.log(count);
+      return classList;
+    }
+  } catch (err) {
+    console.warn(err.message);
+    //ErrorLogTrace('getObjectChildren', err.message);
+  }
 }
+
+console.log(getObjectChildren(document.getElementById('IMP01-01')), {
+  CheckEmpty: false,
+  FetchClass: true,
+});
+
+let ids = [...document.querySelectorAll('.contrib-group [class]')].map(
+  ({ className }) => className
+).filter((v, i, a) => a.indexOf(v) == i).filter(Boolean);
+console.log(ids);
